af3b4e9b2f24413463048ebcf9a936d9fd7a287b
flaizdnag <andrzej.m.gajda@gmail.com>
2024-09-30 08:46:05 +0200

    Token valid for 24 hours

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -210,7 +210,7 @@ async def login_for_access_token(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect login or password.",
         )
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, timedelta(minutes=1440))
 
     return {"access_token": token, "token_type": "bearer"}
 
